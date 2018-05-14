375eca1aa25964844f6e7c427cb2753b540fbdb2
hmscijso <hmscijso@avans.nl>
2018-05-14 13:28:22 +0200

    idadd

--- routes/routes_studentenhuis.js
+++ routes/routes_studentenhuis.js
@@ -3,34 +3,29 @@
 // API - Version 1
 const express   =       require('express');
 const router    =       express.Router();
-const db = require('../db/mysql-connection');
-
-
-
+const db        =       require('../db/mysql-connection.js');
 
 
 router.get('/studentenhuis:id?', (req, resp) => {
 
     const id = req.params.id;
-
-    if( year === '' ) {
-        result = db;
-        query = 'SELECT * FROM studentenhuis';
+    console.log(id);
+    if(!id) {
+        db.query("SELECT * FROM studentenhuis", (err, result, fields) => {
+            if (err) console.error(err);
+            resp.json(result);
+        })
     } else {
-        result = db.filter(function (item) {
-            return (item.info.year === year);
-        });
+        db.query("SELECT * FROM studentenhuis", (err, result, fields) => {
+            if (err) console.error(err);
+            resp.json(result);
+            xd = result.filter(function (item) {
+                return (item.info.id === xd);
+            });
+        })
     }
-    resp.json({
-        'msg': 'test studentenhuis'});
 });
 
-
-//
-// router.get('/studentenhuis/', (req, resp) => {
-//     resp.json({
-//         'msg': 'test studentenhuis'});
-// });
 
 module.exports = router;
 
