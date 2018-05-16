8356a22fdec7bc7d0bb9bee97c929e184e6834f8
teunovic <ttfcolin@avans.nl>
2018-05-16 09:34:19 +0200

    Working usage of Studentenhuis class

--- classes/Studentenhuis.js
+++ classes/Studentenhuis.js
@@ -1,26 +1,46 @@
 class Studentenhuis
 {
-    constructor(id, address, name, userid) {
+    constructor(id, name, address, userid, contact, email) {
         this._id = id;
-        this._address = address;
         this._name = name;
+        this._address = address;
         this._userid = userid;
+        this._contact = contact;
+        this._email = email;
+    }
+
+    get json() {
+        return {
+            "ID": this._id,
+            "naam": this._name,
+            "adres": this._address,
+            "contact": this._contact,
+            "email": this._email,
+        };
     }
 
     get id() {
         return this._id;
     }
 
-    get address() {
-        return this._address;
-    }
-
     get name() {
         return this._name;
     }
 
+    get address() {
+        return this._address;
+    }
+
     get userid() {
         return this._userid;
+    }
+
+    get contact() {
+        return this._contact;
+    }
+
+    get email() {
+        return this._email;
     }
 }
 
--- routes/routes_studentenhuis.js
+++ routes/routes_studentenhuis.js
@@ -10,17 +10,22 @@ router.all('/:id*', (req, res, next) => {
     console.log("ANY studentenhuis request, first get the requested studentenhuis");
     const id = req.params.id;
 
-    db.query("SELECT * FROM studentenhuis WHERE ID = ? LIMIT 1",
+    if(isNaN(id)) {
+        res.status(500).json(util.getError("Ongeldig studentenhuis id")).end();
+        return;
+    }
+
+    db.query("SELECT v.*, s.UserID FROM view_studentenhuis AS v INNER JOIN studentenhuis AS s ON s.ID = v.ID WHERE v.ID = ? LIMIT 1",
         [id],
         (err, results) => {
             if(err) console.error(err);
             else {
                 if(results.length !== 1) {
-                    res.status(500).json(util.getError("Studentenhuis bestaat niet"));
+                    res.status(500).json(util.getError("Studentenhuis bestaat niet")).end();
                     return;
                 }
 
-                let huis = new Studentenhuis(id, results[0]['Naam'], results[0]['Adres'], results[0]['UserID']);
+                let huis = new Studentenhuis(id, results[0]['Naam'], results[0]['Adres'], results[0]['UserID'], results[0]['Contact'], results[0]['Email']);
                 res.locals.huis = huis;
                 return next();
             }
@@ -29,24 +34,16 @@ router.all('/:id*', (req, res, next) => {
 
 router.get('/:id?', (req, res) => {
     const id = req.params.id;
-
-    console.log(res.locals.huis);
-
+    const huis = res.locals.huis;
     console.log("Studentenhuis request from user " + res.locals.user['Voornaam'] + ' ' + res.locals.user['Achternaam']);
-    if(!id) {
+    if(!id || !huis) {
         db.query("SELECT * FROM studentenhuis", (err, result) => {
             if (err) console.error(err);
             res.json(result);
         })
     } else {
-        db.query("SELECT * FROM studentenhuis WHERE id = ? LIMIT 1", [id], (err, result) => {
-            if (err) console.error(err);
-            if(result.length !== 1) {
-                res.status(404).json(util.getError("HuisId bestaat niet"));
-            } else {
-                res.json(result[0]);
-            }
-        })
+        res.json(huis.json).end();
+        return;
     }
 });
 
