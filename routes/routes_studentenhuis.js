a95d3aa41126442ad2df1511c70bd751b3363b8c
teunovic <ttfcolin@avans.nl>
2018-05-16 10:36:02 +0200

    updates on studentenhuis post

--- routes/routes_studentenhuis.js
+++ routes/routes_studentenhuis.js
@@ -50,27 +50,41 @@ router.get('/:id?', (req, res) => {
 //POST-requests || Studentenhuis
 
 
-router.post('', (req, resp) => {
+router.post('^/?$', (req, res) => {
+    console.log("Studentenhuis post from user " + res.locals.user['Voornaam'] + " " + res.locals.user['Achternaam'] + " with UserID " + res.locals.user['ID'])
 
-    console.log("Studentenhuis post from user " + resp.locals.user['Voornaam'] + " " + resp.locals.user['Achternaam'] + " with UserID " + resp.locals.user['ID'])
+    // Checking post body
+    let naam = req.body.naam || '';
+    let adres = req.body.adres || '';
 
-    let info = req.body;
+    if(naam == '' || naam.length > 64 || naam.length < 1) {
+        res.status(412).json(util.getError("De naam moet tussen 1 en 64 tekens zijn", 1)).end();
+        return;
+    }
+
+    if(adres == '' || adres.length > 64 || adres.length < 1) {
+        res.status(412).json(util.getError("Het adres moet tussen 1 en 64 tekens zijn", 2)).end();
+        return;
+    }
+
     const query = {
         sql: 'INSERT INTO `studentenhuis` (Naam, Adres, UserID) VALUES (?, ?, ?)',
-        values: [info.naam, info.adres, resp.locals.user['ID']],
+        values: [naam, adres, res.locals.user['ID']],
         timeout: 2000
     };
     console.log('QUERY: ' + query.sql);
 
-    db.query( query, (error, rows, fields) => {
+    db.query(query, (error, rows, fields) => {
         if (error) {
-            resp.status(500).json(error.toString())
+            res.status(500).json(util.getError(error.toString(), -1))
         } else {
-            resp.status(200);
-            db.query("SELECT * FROM view_studentenhuis INNER JOIN studentenhuis ON studentenhuis.ID = view_studentenhuis.ID WHERE UserID = ?", [resp.locals.user['ID']], (err, result, fields) => {
-                if (err) console.error(err);
-                resp.json(result[0]);
-            })
+            db.query("SELECT * FROM view_studentenhuis WHERE ID = ? LIMIT 1",
+                [rows.insertId],
+                (err, result, fields) => {
+                    if (err) console.error(err);
+                    res.json(result[0]);
+                }
+            );
 
         }
     })
