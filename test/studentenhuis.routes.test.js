b373d129bb313fb5f240bfad98ab227a8aa91dff
hmscijso <hmscijso@avans.nl>
2018-05-16 16:51:11 +0200

    removed 2 last delete tests / irrelevant

--- test/studentenhuis.routes.test.js
+++ test/studentenhuis.routes.test.js
@@ -257,18 +257,4 @@ describe('Studentenhuis API DELETE', () => {
         //
         done()
     });
-
-    it('should throw an error when naam is missing', (done) => {
-        //
-        // Hier schrijf je jouw testcase.
-        //
-        done()
-    });
-
-    it('should throw an error when adres is missing', (done) => {
-        //
-        // Hier schrijf je jouw testcase.
-        //
-        done()
-    })
 });
