0c60f0a6a01cda10d7707c7b674ec4f3f0675d79
teunovic <ttfcolin@avans.nl>
2018-05-16 21:05:42 +0200

    Quick debug line

--- db/mysql-connection.js
+++ db/mysql-connection.js
@@ -1,12 +1,17 @@
 const dbconf = require('./dbconfig');
 const mysql = require('mysql');
 
-let con = mysql.createConnection({
+let settings = {
     host: process.env.DB_HOST || dbconf.dbServer,
     user: process.env.DB_USER || dbconf.dbUsername,
     password: process.env.DB_PASSWORD || dbconf.dbPassword,
     database: process.env.DB_DATABASE || dbconf.dbSchema
-});
+};
+
+console.log("Starting connection...");
+console.log(settings);
+
+let con = mysql.createConnection(settings);
 
 con.connect(function(err) {
     if (err) throw err;
