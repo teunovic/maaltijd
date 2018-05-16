9c014bc7057df9a07767842406e38e2be2c407d6
teunovic <ttfcolin@avans.nl>
2018-05-16 10:13:56 +0200

    Working on deelnemer post

--- routes/routes_deelnemers.js
+++ routes/routes_deelnemers.js
@@ -29,12 +29,12 @@ router.post('/:id/maaltijd/:maaltijdid/deelnemers', (req, resp) => {
 
     console.log("Deelnemers POST-request from user " + resp.locals.user['Voornaam'] + " " + resp.locals.user['Achternaam'] + " with UserID " + resp.locals.user['ID']);
 
-    const id = req.params.id;
+    const huis = resp.locals.huis;
     const maaltijdid = req.params.maaltijdid;
 
     const query = {
         sql: 'INSERT INTO `deelnemers` (UserID, StudentenhuisID, MaaltijdID) VALUES (?, ?, ?)',
-        values: [resp.locals.user['ID'], id, maaltijdid],
+        values: [resp.locals.user['ID'], huis.id, maaltijdid],
         timeout: 2000
     };
     console.log('QUERY: ' + query.sql);
@@ -47,16 +47,10 @@ router.post('/:id/maaltijd/:maaltijdid/deelnemers', (req, resp) => {
         }
     });
     console.log("User " + resp.locals.user['Voornaam'] + " " + resp.locals.user['Achternaam'] + " with UserID " + resp.locals.user['ID'] + " successfully applied for meal " +
-        maaltijdid + " in student house with ID " + id);
-    db.query("SELECT Voornaam, Achternaam, Email FROM `user` WHERE ID = ?", [resp.locals.user['ID']], (err, result, fields) =>{
-        if (err) {
-            console.error(err);
-            //TODO: Send error
-        }
-        else{
-            resp.status(200).json(result[0]);
-        }
-    });
+        maaltijdid + " in student house with ID " + huis.id);
+
+
+
 });
 
 
